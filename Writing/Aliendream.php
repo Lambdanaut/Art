09e751234df872a2c36989158419bcb2d360ef0b
Josh Thomas <personettelabs@gmail.com>
2021-02-18 12:00:22 -0500

    adds writing

---
+++ Writing/Aliendream.php
@@ -0,0 +1,17 @@
+[10:40:06 AM] Grandpa Josh: I had this dream where I was in a space station and there was this computer voice talking to me from the speakers
+[10:40:10 AM] Grandpa Josh: and it was telling me to do things
+[10:40:26 AM] beyonce: isn't hotline miami kind of like that
+[10:40:29 AM] Grandpa Josh: nah
+[10:40:32 AM] beyonce: tellin u to do stuff
+[10:40:36 AM] Grandpa Josh: not really
+[10:40:51 AM] Grandpa Josh: system shock 2 is a bit tho
+[10:42:30 AM] Grandpa Josh: I kept blacking out and entering rooms with tons of people that had little cute fat alien things attached to their heads that looked kinda like this, http://img.pokemondb.net/artwork/munna.jpg but more veiny and transparent and they didn't have the flower pattern. So like a disgusting one of those.
+[10:42:36 AM] Grandpa Josh: and the people were blacked out and just floating there
+[10:42:43 AM] Grandpa Josh: and the things were like parasites sucking on their heads
+[10:43:27 AM] Grandpa Josh: and over time I learned that the AI that controlled the ship was doing this to them, and I tried to kill it.
+[10:43:33 AM] Grandpa Josh: But right before I killed it, I blacked out a final time
+[10:43:47 AM] Grandpa Josh: and then when I woke up, I realized that the computer didn't exist at all, and it was just a voice in my head
+[10:43:49 AM] Grandpa Josh: and I was insane
+[10:43:55 AM] Grandpa Josh: and I had put the parasites on everyone's head
+[10:44:23 AM] Grandpa Josh: and I was swinging an axe at the computer's core right before I blacked out, but when I woke up the axe was plunged in my head, and I was bleeding out, and then I died
+[10:44:25 AM] Grandpa Josh: and woke up irl
